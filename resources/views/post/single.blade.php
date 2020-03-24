79ec16569175ae574ffe213d63e8bda555362004
fsrocha.dev <contato.frankrocha@gmail.com>
2020-03-24 11:19:12 -0300

    creating ratings migration

--- resources/views/post/single.blade.php
+++ resources/views/post/single.blade.php
@@ -1,79 +1,79 @@
 @extends('layout.app')
 
 @section('title')
-  {{ $post->title }}
+{{ $post->title }}
 @endsection
 
 @section('header')
-  <p>{{ $post->title }}</p>
+<p>{{ $post->title }}</p>
 
-  @parent
+@parent
 @endsection
 
 @section('content')
-   <!-- Post Content -->
-   <article>
+<!-- Post Content -->
+<article>
     <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          {{ $post->content }}
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                {{ $post->content }}
+            </div>
         </div>
-      </div>
     </div>
-  </article>
+</article>
 
-  <div class="container">
-      <div class="row">
+<div class="container">
+    <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <p>
-            @foreach($post->categories as $category)
-              <a href="#">{{ $category->name }}</a>
-            @endforeach
-          </p>
+            <p>
+                @foreach($post->categories as $category)
+                <a href="#">{{ $category->name }}</a>
+                @endforeach
+            </p>
         </div>
-      </div>
-  </div>
+    </div>
+</div>
 
 
-  <hr>
+<hr>
 
-  <div class="container">
-      <div class="row">
+<div class="container">
+    <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <h3>Comentários</h3>
+            <h3>Comentários</h3>
         </div>
 
         @foreach($post->comments as $comment)
-          <div class="col-lg-8 col-md-10 mx-auto">
-          <h6>{{ $comment->title }}</h6>
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <h6>{{ $comment->title }}</h6>
             <p style="font-size: 15px; margin-top: 5px">
-              {{ $comment->content }}
+                {{ $comment->content }}
             </p>
-          </div>
-        @endforeach  
+        </div>
+        @endforeach
 
-      </div>
-  </div>
+    </div>
+</div>
 
-  <div class="container">
+<div class="container">
     <form action="{{ route('comments.store', $post) }}" method="post">
-      {{ csrf_field() }}
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-            <label for="title">Comentar</label>
-            <input class="form-control" type="text" name="title" placeholder="Assunto">
-        </div>
-      
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <textarea class='form-control' name="content" cols="30" rows="5" placeholder="Mensagem"></textarea>
-        </div>
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <label for="title">Comentar</label>
+                <input class="form-control" type="text" name="title" placeholder="Assunto">
+            </div>
 
-        <div class="col-lg-8 col-md-10 mx-auto text-right">
-          <button type="submit">Enviar</button>
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <textarea class='form-control' name="content" cols="30" rows="5" placeholder="Mensagem"></textarea>
+            </div>
+
+            <div class="col-lg-8 col-md-10 mx-auto text-right">
+                <button type="submit">Enviar</button>
+            </div>
         </div>
-      </div>
-  </form>
-  </div>
+    </form>
+</div>
 
 
 
