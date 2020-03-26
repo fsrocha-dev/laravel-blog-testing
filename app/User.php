de1b58607b659ad1bb9a8e7dd9a4900591396778
fsrocha-dev <contato.frankrocha@gmail.com>
2020-03-26 09:11:16 -0300

    bugfix User model

--- app/User.php
+++ app/User.php
@@ -1,34 +1,4 @@
 <?php
-
-namespace App;
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
@@ -66,6 +36,4 @@ class User extends Authenticatable
     {
         return $this->morphMany('App\Rating', 'ratingable');
     }
-}
-
 }
