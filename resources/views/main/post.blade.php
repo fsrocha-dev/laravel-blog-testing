2934e8d35ea57a8e66423a144e1185866498553f
fsrocha-dev <contato.frankrocha@gmail.com>
2020-03-27 11:04:24 -0300

    add debugbar

--- resources/views/main/post.blade.php
+++ resources/views/main/post.blade.php
@@ -1,23 +1,23 @@
 <div class="post-preview">
-  <a href="{{ route('posts.public.show', $post) }}">
-    <h2 class="post-title">
-      {{ $post->title }}
-    </h2>
-    <h3 class="post-subtitle">
-        {{ $post->content }}
-    </h3>
-  </a>
-  <p class="post-meta">
-    Postado por <a href="#">Treinaweb</a> em @datebr($post->created_at)
-  </p>
-
-  @if (request()->query('categories'))
+    <a href="{{ route('posts.public.show', $post) }}">
+        <h2 class="post-title">
+            {{ $post->title }}
+        </h2>
+        <h3 class="post-subtitle">
+            {{ $post->content }}
+        </h3>
+    </a>
     <p class="post-meta">
-        Categorias: 
+        Postado por <a href="#">Frank Rocha</a> em @datebr($post->created_at)
+    </p>
+
+    @if (request()->query('categories'))
+    <p class="post-meta">
+        Categorias:
         @foreach($post->categories as $category)
-          <a href="#">{{ $category->name }}</a>
+        <a href="#">{{ $category->name }}</a>
         @endforeach
     </p>
-  @endif  
+    @endif
 </div>
 <hr>
--- resources/views/post/single.blade.php
+++ resources/views/post/single.blade.php
@@ -45,6 +45,7 @@
     @endif
     <form action="{{ route('ratings.store', $post) }}" method="post">
         {{ csrf_field() }}
+        @include('layuout.teste')
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
                 <p>A média de avalições é: {{ $post->ratings()->avg('value') }}</p>
