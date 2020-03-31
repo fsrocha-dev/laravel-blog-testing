f623560c1ebc9f2a06398de7a7815f07ecd058a4
fsrocha-dev <contato.frankrocha@gmail.com>
2020-03-31 17:26:22 -0300

    add mutator uc first comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,15 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-  protected $fillable = ['title', 'content'];
+    protected $fillable = ['title', 'content'];
 
-  /**
-   * Mapeia o relacionamento com o model de posts
-   *
-   * @return void
-   */
-   public function post()
-   {
-     return $this->belongsTo('App\Post', 'post_id', 'id');
-   }
+    /**
+     * Mapeia o relacionamento com o model de posts
+     *
+     * @return void
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Post', 'post_id', 'id');
+    }
+
+    /**
+      * Mutator que coloca titulo com primeira letra maiuscula
+      *
+      * @param [type] $value
+      * @return void
+      */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = ucfirst($value);
+    }
 }
